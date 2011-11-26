a2a49fcb1ff9a4edb7377b63c6037c59bfc84f27
andrerocker <andre.souza@gmail.com>
2011-11-26 01:07:01 -0200

    nova formataçao de log

--- lib/logger.rb
+++ lib/logger.rb
@@ -3,7 +3,13 @@ require "logger"
 module MarvBot
   module Logger
     def log
-      @logger ||= ::Logger.new(STDOUT)
+      @logger ||= begin
+        logger = ::Logger.new(STDOUT)
+        logger.formatter = proc do |severity, datetime, progname, msg|
+          "#{severity} #{datetime.strftime("%d/%m/%Y %H:%M:%S")} || #{msg}\n"
+        end
+        logger
+      end
     end
   end
 end
