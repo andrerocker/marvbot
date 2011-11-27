0ebe4b86ba0e636e85391d844d647f9b2289868d
andrerocker <andre.souza@gmail.com>
2011-11-27 20:24:20 -0200

    agora utiliza o router internamente no metodo interact

--- lib/irc.rb
+++ lib/irc.rb
@@ -1,9 +1,24 @@
 module MarvBot
   module IRC
+    include MarvBot::Router
+    
     def start(nickname, room)
       irc %( NICK #{nickname}
              USER #{nickname} 0 * #{nickname.capitalize}
              JOIN ##{room} )
+    end
+    
+    def interact(message)
+      route(message).each do |category|
+        category.each do |service|
+          response = service.execute
+          irc response if response
+        end
+      end
+    end
+    
+    def stop
+      EM.stop_event_loop
     end
 
     def irc(command)
