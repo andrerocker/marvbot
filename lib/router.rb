71a8fd9ffb50ef7c0a9611fc3c30aa12512b1b32
andrerocker <andre.souza@gmail.com>
2011-11-27 20:40:35 -0200

    melhorando um pouco a implementação do método route

--- lib/router.rb
+++ lib/router.rb
@@ -2,12 +2,12 @@ module MarvBot
   module Router
     def route(message_bundle)
       routed = []
-      (messages(message_bundle).each do |message|
+      messages(message_bundle).each do |message|
         routed << (services.collect do |service|
           matched = service.match?(message)
           service.new(message, matched) if matched
-        end).delete_if {|service| service.nil?}
-      end).delete_if {|service| service.nil?}
+        end).compact
+      end
       routed
     end
 
