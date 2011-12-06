15fb3aee031ec73c31f55a67e87944c56e88a827
andrerocker <andre.souza@gmail.com>
2011-12-06 20:21:34 -0200

    agora modulos tambem podem ser adicionados como services a fim de ser um pacote

--- lib/services/troll.rb
+++ lib/services/troll.rb
@@ -1,20 +1,24 @@
 # encoding: utf-8
-class Troll < MarvBot::Plugin
-  match /:([^!]+)!.*PRIVMSG ##{MarvBot.channel} :!troll ([^!]+)$/
+module Troll
+  class Process < MarvBot::Plugin
+    match /:([^!]+)!.*PRIVMSG ##{MarvBot.channel} :!troll ([^!]+)$/
 
-  def execute
-    trollador = matched[1]
-    trollado  = matched[2]
+    def execute
+      trollador = matched[1]
+      trollado  = matched[2]
 
-    if trollado.eql? MarvBot.nickname
-      response trollador, "não sou idiota o suficiente para praticar o self-trolling"
-    else
-      response trollado, "iaeee troxa! hahaahaha!"
+      if trollado.eql? MarvBot.nickname
+        response trollador, "não sou idiota o suficiente para praticar o self-trolling"
+      else
+        response trollado, "iaeee troxa! hahaahaha!"
+      end
     end
+
+    private
+      def response(trollador, message)
+        "PRIVMSG ##{MarvBot.channel} :#{trollador}: #{message}"
+      end
   end
 
-  private
-    def response(trollador, message)
-      "PRIVMSG ##{MarvBot.channel} :#{trollador}: #{message}"
-    end
+  MarvBot::Service.register Process
 end
