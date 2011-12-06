fe2327065591849cb17d451ec74457edd3a61fb6
andrerocker <andre.souza@gmail.com>
2011-12-06 20:51:21 -0200

    adicionando rocker_accessor para basic_object

--- lib/ext/basic_object.rb
+++ lib/ext/basic_object.rb
@@ -2,4 +2,9 @@ require "lib/core/logger"
 
 class BasicObject
   include ::MarvBot::Logger
+
+  def rocker_accessor(sym, value)
+    attr_accessor sym
+    instance_variable_set("@#{sym}", value)
+  end
 end
